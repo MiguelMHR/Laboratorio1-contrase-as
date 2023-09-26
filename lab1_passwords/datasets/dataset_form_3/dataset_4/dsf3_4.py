19223519b71a2d900c2178d951a929091febedb6
helena <100451026@alumnos.uc3m.es>
2023-09-26 14:03:33 +0200

    dataset 2,3 y 4

---
+++ lab1_passwords/datasets/dataset_form_3/dataset_4/dsf3_4.py
@@ -0,0 +1,24 @@
+import hashlib
+import random
+
+# Genera 100 contraseñas de 6 letras aleatorias
+passwords =[]
+for i in range(100):
+    password = ''.join(random.choice('0123456789') for j in range(6))
+    passwords.append(password)
+
+# Hasheas las contraseñas usando md5
+hashed_passwords = []
+for password in passwords:
+    hashed_password = hashlib.md5(password.encode()).hexdigest()
+    hashed_passwords.append(hashed_password)
+
+# Write the original passwords to a txt file
+with open('passwords.txt', 'w') as f:
+    for password in passwords:
+        f.write(password + '\n')
+
+# Write the hashed passwords to a txt file
+with open('hashed_passwords.txt', 'w') as f:
+    for hashed_password in hashed_passwords:
+        f.write(hashed_password + '\n')
---
+++ lab1_passwords/datasets/dataset_form_4/dataset_1/dsf4_1.py
@@ -0,0 +1,25 @@
+import hashlib
+import random
+
+# Genera 100 contraseñas de 3 numeros aleatorias
+
+passwords =[]
+for i in range(100):
+    password = ''.join(random.choice('0123456789') for j in range(3))
+    passwords.append(password)
+
+# Hasheas las contraseñas usando md5
+hashed_passwords = []
+for password in passwords:
+    hashed_password = hashlib.md5(password.encode()).hexdigest()
+    hashed_passwords.append(hashed_password)
+
+# Write the original passwords to a txt file
+with open('passwords.txt', 'w') as f:
+    for password in passwords:
+        f.write(password + '\n')
+
+# Write the hashed passwords to a txt file
+with open('hashed_passwords.txt', 'w') as f:
+    for hashed_password in hashed_passwords:
+        f.write(hashed_password + '\n')
