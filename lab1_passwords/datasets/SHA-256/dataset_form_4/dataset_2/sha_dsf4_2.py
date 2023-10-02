db9ed6687d0d071bf9e65fd2cb3964f27693eec6
MiguelMHR <100451258@alumnos.uc3m.es>
2023-10-02 20:29:18 +0200

    All sha256 datasets done

---
+++ lab1_passwords/datasets/SHA-256/dataset_form_4/dataset_2/sha_dsf4_2.py
@@ -0,0 +1,49 @@
+"""
+Dataset de contraseñas de 4 caracteres, ya sean números, letras
+mayúsculas y minúsculas y símbolos
+"""
+
+# Librerías necesarias:
+import random
+import string
+from Crypto.Hash import SHA256
+from Crypto.Protocol.KDF import bcrypt
+from base64 import b64encode
+
+# Variables globales:
+caracteres = string.digits + string.ascii_letters + string.punctuation
+passwords = []
+hashed_passwords = []
+
+# Genera 100 contraseñas de 4 caracteres aleatorios
+for i in range(100):
+    pw = ''
+    for j in range(4):
+        random_pos = random.randint(0, len(caracteres) - 1)
+        pw += caracteres[random_pos]
+    passwords.append(pw)
+
+""" 
+Hasheas las contraseñas usando SHA-256:
+➡️ Usando bcrypt te genera un salt aleatorio
+"""
+for password in passwords:
+    # Generamos en hash en base64 (bytestring) para que bcrypt pueda aplicar el salt
+    # Además, el password se pasa previamente a bytestring para que se pueda aplicar
+    # la función SHA-256
+    hashed_password = b64encode(SHA256.new(password.encode('utf-8')).digest())
+    # Aplicamos bcrypt para generar el salt aleatorio (generado en cada uso de bcrypt) 
+    # y el hash final
+    # NOTA: En la documentación, se recomienda usar un cost de 12 
+    bcrypt_hashed_password = bcrypt(hashed_password, 12)
+    hashed_passwords.append(bcrypt_hashed_password.decode('utf-8'))
+
+# Escribimos las contraseñas originales en un txt
+with open('passwords.txt', 'w') as f:
+    for password in passwords:
+        f.write(password + '\n')
+
+# Escribimos los hashes con salt en un txt
+with open('hashed_passwords.txt', 'w') as f:
+    for hashed_password in hashed_passwords:
+        f.write(hashed_password + '\n')
---
+++ lab1_passwords/datasets/SHA-256/dataset_form_5/dataset_5/sha_dsf5_5.py
@@ -0,0 +1,55 @@
+"""
+Dataset Diccionarios 5: 
+- contraseñas de palabras de max 7 que contengan
+  combinaciones de solo consonantes más comunes
+  en inglés
+- Ejemplos: 'th', 'ch', 'sh', 'wh', 'ph', 'kn', 'wr'
+"""
+
+# Librerías necesarias:
+from random_word import RandomWords
+from Crypto.Hash import SHA256
+from Crypto.Protocol.KDF import bcrypt
+from base64 import b64encode
+
+# Variables globales:
+r = RandomWords()
+comb_letters = ['th', 'ch', 'sh', 'wh', 'ph']
+passwords = []
+hashed_passwords = []
+
+# Genera 100 contraseñas de palabras aleatorias de 3 caracteres
+for i in range(100):
+    found = False
+    while not found:
+        r_word = r.get_random_word()
+        for i in range(len(r_word)-1):
+            if r_word[i:i+2] in comb_letters and len(r_word) <= 7:
+                found = True
+    print(r_word)
+    passwords.append(r_word)
+
+""" 
+Hasheas las contraseñas usando SHA-256:
+➡️ Usando bcrypt te genera un salt aleatorio
+"""
+for password in passwords:
+    # Generamos en hash en base64 (bytestring) para que bcrypt pueda aplicar el salt
+    # Además, el password se pasa previamente a bytestring para que se pueda aplicar
+    # la función SHA-256
+    hashed_password = b64encode(SHA256.new(password.encode('utf-8')).digest())
+    # Aplicamos bcrypt para generar el salt aleatorio (generado en cada uso de bcrypt) 
+    # y el hash final
+    # NOTA: En la documentación, se recomienda usar un cost de 12 
+    bcrypt_hashed_password = bcrypt(hashed_password, 12)
+    hashed_passwords.append(bcrypt_hashed_password.decode('utf-8'))
+
+# Escribimos las contraseñas originales en un txt
+with open('passwords.txt', 'w') as f:
+    for password in passwords:
+        f.write(password + '\n')
+
+# Escribimos los hashes con salt en un txt
+with open('hashed_passwords.txt', 'w') as f:
+    for hashed_password in hashed_passwords:
+        f.write(hashed_password + '\n')
