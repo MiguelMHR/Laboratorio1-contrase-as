25a85c029fbc25bafe013c2c9483ef5b29a0408f
MiguelMHR <100451258@alumnos.uc3m.es>
2023-10-02 11:21:48 +0200

    Actualizados datasets de tipo 2

--- lab1_passwords/datasets/dataset_form_2/dataset_1/dsf2_1.py
+++ lab1_passwords/datasets/dataset_form_2/dataset_1/dsf2_1.py
@@ -1,24 +1,47 @@
-import hashlib
+"""
+Dataset de contraseñas de 3 letras mayúsculas
+"""
+
+# Librerías necesarias:
 import random
+from Crypto.Hash import MD5
+from Crypto.Protocol.KDF import bcrypt
+from base64 import b64encode
+
+# Variables globales:
+letras_minusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZ'
+passwords = []
+hashed_passwords = []
 
 # Genera 100 contraseñas de 3 letras aleatorias
-passwords = []
 for i in range(100):
-    password = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for j in range(3))
-    passwords.append(password)
+    pw = ''
+    for j in range(3):
+        random_letter_pos = random.randint(0, 25)
+        pw += letras_minusculas[random_letter_pos]
+    passwords.append(pw)
 
-# Hasheas las contraseñas usando md5
-hashed_passwords = []
+""" 
+Hasheas las contraseñas usando MD5:
+➡️ Usando bcrypt te genera un salt aleatorio
+"""
 for password in passwords:
-    hashed_password = hashlib.md5(password.encode()).hexdigest()
-    hashed_passwords.append(hashed_password)
+    # Generamos en hash en base64 (bytestring) para que bcrypt pueda aplicar el salt
+    # Además, el password se pasa previamente a bytestring para que se pueda aplicar
+    # la función MD5
+    hashed_password = b64encode(MD5.new(password.encode('utf-8')).digest())
+    # Aplicamos bcrypt para generar el salt aleatorio (generado en cada uso de bcrypt) 
+    # y el hash final
+    # NOTA: En la documentación, se recomienda usar un cost de 12 
+    bcrypt_hashed_password = bcrypt(hashed_password, 12)
+    hashed_passwords.append(bcrypt_hashed_password.decode('utf-8'))
 
-# Write the original passwords to a txt file
+# Escribimos las contraseñas originales en un txt
 with open('passwords.txt', 'w') as f:
     for password in passwords:
         f.write(password + '\n')
 
-# Write the hashed passwords to a txt file
+# Escribimos los hashes con salt en un txt
 with open('hashed_passwords.txt', 'w') as f:
     for hashed_password in hashed_passwords:
         f.write(hashed_password + '\n')
