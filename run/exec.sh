273ab9f0e2b9b5cc8189539b317605fac88975a6
MiguelMHR <100451258@alumnos.uc3m.es>
2023-10-22 18:11:26 +0200

    Script updated

--- run/exec.sh
+++ run/exec.sh
@@ -15,26 +15,26 @@ if [ -f ~/snap/john-the-ripper/current/.john/john.pot ]; then
 fi
 
 # Comando para borrar el results.log si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log
 fi
 
 # Comando para borrar el mean_medium.txt si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/mean_medium.txt ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/mean_medium.txt
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/mean_medium.txt ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/mean_medium.txt
 fi
 
 # Comando para ejecutar john-the-ripper estrategia 3 con MD5 con timeout de 10 mins
-timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/dsf1_1_hashed_passwords.txt
+timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/dsf1_1_hashed_passwords.txt
 
 # Comando para enseñar los resultados
-john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/dsf1_1_hashed_passwords.txt
+john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/dsf1_1_hashed_passwords.txt
 
 # Comando para mover el john.log a resources y cambiar el nombre a results.log
-mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log
+mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log
 
 # Comando para ejecutar script de python media y moda
-python3 ~/ClonedRepositories/IngCiber/lab1_passwords/resources/mean_medium.py ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log >> ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_1/resources/mean_medium.txt
+python3 ~/ClonedRepositories/Laboratorio1-passwords/run/mean_medium.py ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/results.log >> ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_1/resources/mean_medium.txt
 
 ################     DATASET FORM 1 - DATASET 2     ################
 # Comando para borrar el john.pot si existe
@@ -43,26 +43,26 @@ if [ -f ~/snap/john-the-ripper/current/.john/john.pot ]; then
 fi
 
 # Comando para borrar el results.log si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log
 fi
 
 # Comando para borrar el mean_medium.txt si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/mean_medium.txt ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/mean_medium.txt
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/mean_medium.txt ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/mean_medium.txt
 fi
 
 # Comando para ejecutar john-the-ripper estrategia 3 con MD5 con timeout de 10 mins
-timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/dsf1_2_hashed_passwords.txt
+timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/dsf1_2_hashed_passwords.txt
 
 # Comando para enseñar los resultados
-john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/dsf1_2_hashed_passwords.txt
+john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/dsf1_2_hashed_passwords.txt
 
 # Comando para mover el john.log a resources y cambiar el nombre a results.log
-mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log
+mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log
 
 # Comando para ejecutar script de python media y moda
-python3 ~/ClonedRepositories/IngCiber/lab1_passwords/resources/mean_medium.py ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log >> ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_2/resources/mean_medium.txt
+python3 ~/ClonedRepositories/Laboratorio1-passwords/run/mean_medium.py ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/results.log >> ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_2/resources/mean_medium.txt
 
 ################     DATASET FORM 1 - DATASET 3     ################
 # Comando para borrar el john.pot si existe
@@ -71,26 +71,26 @@ if [ -f ~/snap/john-the-ripper/current/.john/john.pot ]; then
 fi
 
 # Comando para borrar el results.log si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log
 fi
 
 # Comando para borrar el mean_medium.txt si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/mean_medium.txt ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/mean_medium.txt
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/mean_medium.txt ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/mean_medium.txt
 fi
 
 # Comando para ejecutar john-the-ripper estrategia 3 con MD5 con timeout de 10 mins
-timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/dsf1_3_hashed_passwords.txt
+timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/dsf1_3_hashed_passwords.txt
 
 # Comando para enseñar los resultados
-john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/dsf1_3_hashed_passwords.txt
+john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/dsf1_3_hashed_passwords.txt
 
 # Comando para mover el john.log a resources y cambiar el nombre a results.log
-mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log
+mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log
 
 # Comando para ejecutar script de python media y moda
-python3 ~/ClonedRepositories/IngCiber/lab1_passwords/resources/mean_medium.py ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log >> ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_3/resources/mean_medium.txt
+python3 ~/ClonedRepositories/Laboratorio1-passwords/run/mean_medium.py ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/results.log >> ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_3/resources/mean_medium.txt
 
 ################     DATASET FORM 1 - DATASET 4     ################
 # Comando para borrar el john.pot si existe
@@ -99,26 +99,26 @@ if [ -f ~/snap/john-the-ripper/current/.john/john.pot ]; then
 fi
 
 # Comando para borrar el results.log si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log
 fi
 
 # Comando para borrar el mean_medium.txt si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/mean_medium.txt ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/mean_medium.txt
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/mean_medium.txt ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/mean_medium.txt
 fi
 
 # Comando para ejecutar john-the-ripper estrategia 3 con MD5 con timeout de 10 mins
-timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/dsf1_4_hashed_passwords.txt
+timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/dsf1_4_hashed_passwords.txt
 
 # Comando para enseñar los resultados
-john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/dsf1_4_hashed_passwords.txt
+john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/dsf1_4_hashed_passwords.txt
 
 # Comando para mover el john.log a resources y cambiar el nombre a results.log
-mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log
+mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log
 
 # Comando para ejecutar script de python media y moda
-python3 ~/ClonedRepositories/IngCiber/lab1_passwords/resources/mean_medium.py ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log >> ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_4/resources/mean_medium.txt
+python3 ~/ClonedRepositories/Laboratorio1-passwords/run/mean_medium.py ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/results.log >> ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_4/resources/mean_medium.txt
 
 ################     DATASET FORM 1 - DATASET 5     ################
 # Comando para borrar el john.pot si existe
@@ -127,23 +127,23 @@ if [ -f ~/snap/john-the-ripper/current/.john/john.pot ]; then
 fi
 
 # Comando para borrar el results.log si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log
 fi
 
 # Comando para borrar el mean_medium.txt si existe
-if [ -f ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/mean_medium.txt ]; then
-    rm ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/mean_medium.txt
+if [ -f ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/mean_medium.txt ]; then
+    rm ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/mean_medium.txt
 fi
 
 # Comando para ejecutar john-the-ripper estrategia 3 con MD5 con timeout de 10 mins
-timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/dsf1_5_hashed_passwords.txt
+timeout 600 john-the-ripper --wordlist=/home/midoosh/Descargas/rockyou.txt --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/dsf1_5_hashed_passwords.txt
 
 # Comando para enseñar los resultados
-john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/dsf1_5_hashed_passwords.txt
+john-the-ripper --show --format=raw-md5 ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/dsf1_5_hashed_passwords.txt
 
 # Comando para mover el john.log a resources y cambiar el nombre a results.log
-mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log
+mv ~/snap/john-the-ripper/current/.john/john.log ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log
 
 # Comando para ejecutar script de python media y moda
-python3 ~/ClonedRepositories/IngCiber/lab1_passwords/resources/mean_medium.py ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log >> ~/ClonedRepositories/IngCiber/lab1_passwords/datasets/MD5/dataset_form_1/dataset_5/resources/mean_medium.txt
+python3 ~/ClonedRepositories/Laboratorio1-passwords/run/mean_medium.py ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/results.log >> ~/ClonedRepositories/Laboratorio1-passwords/datasets/MD5/dataset_form_1/dataset_5/resources/mean_medium.txt
